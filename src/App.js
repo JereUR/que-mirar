8381d8f3725fb6bbcf87a1e43a8d1ac2554d334f
Jeremias Dominguez Vega <jeremias.jdv@gmail.com>
2022-05-08 15:35:57 -0300

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Form } from "./components/Form";
+
+function App() {
+  return (
+    <div>
+      <h1>QueMirar.com</h1>
+      <Form></Form>
+    </div>
+  );
+}
+export default App;
