69c07edbfdfbc3e53ef913962047b4d69092ead6
JereUR <jereur.jdv@gmail.com>
2022-07-14 06:04:39 -0300

    Styles fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export const Header = () => {
       <section class="header-content container">
         <article class="logo">
           <img
-            src="https://cdn.discordapp.com/attachments/894702938386210896/997024365529014302/Logo.png"
+            src="https://cdn.discordapp.com/attachments/894702938386210896/997062230539501609/Logo.png"
             alt="logo"
           />
           <h1>QueMirar.com</h1>
